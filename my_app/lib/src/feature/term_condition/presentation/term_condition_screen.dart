15881ae0fb043c019608b44a7ec6cfc84c49223b
s95848 <sophon.jumpapang@scb.co.th>
2023-09-19 12:34:30 +0700

    redirect to home screen

--- my_app/lib/src/feature/term_condition/presentation/term_condition_screen.dart
+++ my_app/lib/src/feature/term_condition/presentation/term_condition_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_app/src/feature/home/presentation/home_screen.dart';
 import 'package:my_app/src/feature/term_condition/presentation/expansion_tile_widget.dart';
 
 class TermCondition extends StatelessWidget {
@@ -12,13 +13,21 @@ class TermCondition extends StatelessWidget {
         ),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.start,
-          children: const [
-            ExpansionTileWidget(
+          children: [
+            const ExpansionTileWidget(
               title: 'Term condition 1',
               initiallyExpanded: true,
             ),
-            ExpansionTileWidget(title: 'Term condition 2'),
-            ExpansionTileWidget(title: 'Term condition 3'),
+            const ExpansionTileWidget(title: 'Term condition 2'),
+            const ExpansionTileWidget(title: 'Term condition 3'),
+            OutlinedButton(
+              onPressed: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                  builder: (_) => const HomeScreen(),
+                ));
+              },
+              child: const Text('Submit'),
+            )
           ],
         ));
   }
