479a2d79018f38cfabedf6e0f1854fb6fa1b86d4
s95848 <sophon.jumpapang@scb.co.th>
2023-09-19 12:34:38 +0700

    add home screen

---
+++ my_app/lib/src/feature/home/presentation/all_menu_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class AllMenu extends StatefulWidget {
+  const AllMenu({Key? key}) : super(key: key);
+
+  @override
+  AllMenuState createState() => AllMenuState();
+}
+
+class AllMenuState extends State<AllMenu> {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text("AllMenu"));
+  }
+}
---
+++ my_app/lib/src/feature/home/presentation/home_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:my_app/src/feature/home/presentation/all_menu_screen.dart';
+import 'package:my_app/src/feature/term_condition/presentation/term_condition_screen.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  HomeScreenState createState() => HomeScreenState();
+}
+
+class HomeScreenState extends State<HomeScreen> {
+  List<Widget> data = [const AllMenu()];
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoTabScaffold(
+      tabBar: CupertinoTabBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.person),
+            label: "Profile",
+          )
+        ],
+      ),
+      tabBuilder: (context, index) {
+        return CupertinoTabView(
+          builder: (context) {
+            return data[index];
+          },
+        );
+      },
+    );
+  }
+}
